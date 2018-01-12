eadcd85b950fef2523f4c09084fb4c69d6ab483b
Jordan Martinez <jordanalex.martinez@gmail.com>
2018-01-12 00:45:47 -0800

    Document most of the project's public API

--- src/main/java/org/fxmisc/wellbehaved/event/InputHandler.java
+++ src/main/java/org/fxmisc/wellbehaved/event/InputHandler.java
@@ -3,11 +3,46 @@ package org.fxmisc.wellbehaved.event;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 
+/**
+ * Runs a block of code when its corresponding {@link EventPattern} matches a given event type (e.g. the block of code
+ * to run in a more powerful switch statement)
+ * @param <T> the type of the event that will be passed into {@link #process(Event)}
+ */
 @FunctionalInterface
 public interface InputHandler<T extends Event> extends EventHandler<T> {
 
-    public enum Result { PROCEED, CONSUME, IGNORE }
+    /**
+     * Signifies what to do after handling some input:
+     * <ul>
+     *     <li>
+     *         continue trying to match the event type with the next given {@link EventPattern} ({@link #PROCEED})
+     *     </li>
+     *     <li>
+     *         stop trying to match the event type and consume the event ({@link #CONSUME})
+     *     </li>
+     *     <li>
+     *         stop trying to match the event type and do not consume it ({@link #IGNORE})
+     *     </li>
+     * </ul>
+     */
+    public enum Result {
+        /**
+         * Try to continue to match the event type with the next given {@link EventPattern}. This can be
+         * used to run some code before an {@link InputHandler} that consumes the event.
+         */
+        PROCEED,
+        /** Stop trying to match the event type with the next given {@link EventPattern} and consume the event */
+        CONSUME,
+        /**
+         * Stop trying to match the event type with the next given {@link EventPattern} and do not consume it
+         */
+        IGNORE
+    }
 
+    /**
+     * When the corresponding {@link EventPattern} matches an event type, this method is called. The implementation
+     * does not need to consume the event.
+     */
     Result process(T event);
 
     @Override
---
+++ src/main/java/org/fxmisc/wellbehaved/event/internal/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package is for internal usage is will be made inaccessible in Java 10.
+ */
+package org.fxmisc.wellbehaved.event.internal;
